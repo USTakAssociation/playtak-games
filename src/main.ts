7cd4fc55394765700c0b0ddf78f1148edcb6c3a4
Braydon Harris <braydonharris@gmail.com>
2022-11-14 22:43:36 -0700

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+// Plugins
+import { Quasar } from "quasar";
+
+// Import icon libraries
+import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
+import "quasar/src/css/index.sass";
+
+const app = createApp(App)
+
+app
+  .use(Quasar)
+  .mount('#app')
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,33 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Composables
+import { createVuetify } from "vuetify";
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+	theme: {
+		defaultTheme: "dark",
+		themes: {
+		light: {
+			colors: {
+			primary: "#35478C",
+			secondary: "#5CBBF6",
+			},
+		},
+		dark: {
+			colors: {
+			primary: "#35478C",
+			secondary: "#5CBBF6",
+			},
+		},
+		},
+	},
+});
