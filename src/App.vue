e941167cd7ab6ae6f34b350a6ca151edb55bad23
Braydon Harris <braydonharris@gmail.com>
2023-02-05 22:59:22 -0700

    chore: add note about partial search for usernames chore: html cleanup

--- src/App.vue
+++ src/App.vue
@@ -261,12 +261,16 @@
 					<q-card-section class="row items-center q-pt-none">
 						<p class="q-mt-md">Games before 23rd April 2016 5:00 PM UTC are anonymized and won't appear when searching with player name</p>
 						<p>
-						Please don't scrape this site. You can directly download the database here: <br>
-						<a href="/games_anon.db">Games Database</a>
-						({{dbData.size}} MB) (updated on {{dbData.date}}) <br> 
-						note that the notation is in play tak server format.</p>
+							Please don't scrape this site. You can directly download the database here: <br>
+							<a href="/games_anon.db">Games Database</a>
+							({{dbData.size}} MB) (updated on {{dbData.date}}) <br> 
+							note that the notation is in play tak server format.
+						</p>
 						<p>
-						The link above will be updated with latest database every day at 5:00 PM UTC.
+							The link above will be updated with latest database every day at 5:00 PM UTC.
+						</p>
+						<p>
+							In the player name search fields you can do a partial search with % at the start, end, or both. ie %ame or nam% or %am%
 						</p>
 					</q-card-section>
 				</q-card>
